812f817245687ad9aa2c997573a80865df52a9b2
elfein <elfein99@gmail.com>
2018-10-08 16:47:03 -0400

    enabled react router, rendered index page

---
+++ client/src/components/Splash.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to</h2>
+        <h1>Starlight</h1>
+        <h1>Movie Nights</h1>
+      </div>
+    )
+  }
+}
