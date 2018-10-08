81ffb7c64f69f13ac289aba883c8bcb54ee47c4e
elfein <elfein99@gmail.com>
2018-10-08 15:42:56 -0400

    initial commit with server, mongoose, and react running

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// mongoose setup
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+const connection = mongoose.connection
+connection.on('connected', () => {
+    console.log('Mongoose Connected Successfully')
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err)
+})
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/client/build'));
+
+// root index
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
+// app.use('/users', usersRouter);
+
+module.exports = app;
